a7d0b7ace1b2f9b7d18ed4b7ab4fcf32e3e2e75a
Yuichi Saito <munisystem@gmail.com>
2017-08-02 10:54:45 +0900

    Implement aws session generator

---
+++ aws/aws.go
@@ -0,0 +1,15 @@
+package aws
+
+import "github.com/aws/aws-sdk-go/aws/session"
+
+var (
+	sess *session.Session
+)
+
+func Session() *session.Session {
+	if sess != nil {
+		sess = session.Must(session.NewSession())
+	}
+
+	return sess
+}
